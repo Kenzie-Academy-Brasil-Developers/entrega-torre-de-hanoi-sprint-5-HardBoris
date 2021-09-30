4fbbe4753c81267afb58da9eaf57fda943ac8bc3
HardBoris <bgaibor03@gmail.com>
2021-09-29 22:11:59 -0400

    movimientos sem logica

--- script.js
+++ script.js
@@ -1,42 +1,251 @@
-const hanoi = document.getElementById("hanoi");
+const torres = document.getElementsByClassName("torres");
 
-function Disco(i){
-    this.disco = document.createElement("div");
-    this.disco.className = "disk";
-    this.disco.id = "d" + i;
-}
+const discos = [];
+const fichas = [];
 
-function criaFicha(n) {
-    for (let i = 0; i < n; i++) {
-        let ficha = new Disco(i);
-        const torreA = document.getElementById("a");
-        torreA.appendChild(ficha.disco);
-    }
-}
+const torreA = new Torre("a", true);
+const torreB = new Torre("b", false);
+const torreC = new Torre("c", false);
+
+const fichasA = [];
+const fichasB = [];
+const fichasC = [];
+
+const disco0 = new Disco(0);
+const disco1 = new Disco(1);
+const disco2 = new Disco(2);
+const disco3 = new Disco(3);
+const disco4 = new Disco(4);
+const disco5 = new Disco(5);
+const disco6 = new Disco(6);
+const disco7 = new Disco(7);
+
+const ficha0 = disco0.disco;
+const ficha1 = disco1.disco;
+const ficha2 = disco2.disco;
+const ficha3 = disco3.disco;
+const ficha4 = disco4.disco;
+const ficha5 = disco5.disco;
+const ficha6 = disco6.disco;
+const ficha7 = disco7.disco;
+
+const valor0 = disco0.valor;
+const valor1 = disco1.valor;
+const valor2 = disco2.valor;
+const valor3 = disco3.valor;
+const valor4 = disco4.valor;
+const valor5 = disco5.valor;
+const valor6 = disco6.valor;
+const valor7 = disco7.valor;
+
+discos.push(ficha0);
+discos.push(ficha1);
+discos.push(ficha2);
+discos.push(ficha3);
+discos.push(ficha4);
+discos.push(ficha5);
+discos.push(ficha6);
+discos.push(ficha7);
+
+
 
 function limpaTorre(){
-    // const delFicha = document.getElementsByClassName("disk");
-    // console.log(delFicha)
-    // for (let i = 0; i < delFicha.length; i++) {
-    //     const element = delFicha[i];
-    //     console.log(element)
-    //     element.remove();
-    // }
-    const torres = document.getElementsByClassName("torres");
     for (let i = 0; i < torres.length; i++) {
         const element = torres[i];
         element.innerText = "";
     }
 }
 
+function Disco(i) {
+    this.disco = document.getElementById("d" + i);
+    this.valor = i;
+    this.elegida = false;
+}
+
+// function criaDiscos() {
+//     for (let i = 0; i < 8; i++) {
+//         discos[i] = new Disco(i)
+//     }
+//     return discos;
+// }
+
+// function Fichas(str){
+//     this.fichas = [];
+// }
+
+function Torre(str, inicial){
+    this.torre = document.getElementById(str);
+    this.inicial = inicial;
+    this.elegido = false;
+    
+}
+
+function criaFichas(n) {
+    for (let i = 0; i < n; i++) {
+        fichasA[i] = discos[i];
+    }
+    return fichasA;
+}
+
+function guardaFichas(n) {
+    criaFichas(n);
+    for (let i = 0; i < n; i++) {
+        torreA.torre.appendChild(fichasA[i]);
+    }
+}
+
+// function iniciar() {
+//     const hanoi = document.getElementById("hanoi");
+    
+//     hanoi.appendChild(torreA.torre);
+//     hanoi.appendChild(torreB.torre);
+//     hanoi.appendChild(torreC.torre);
+    
+//     criaDiscos();
+
+    torreA.torre.addEventListener("click", selecionaA, false);
+    torreB.torre.addEventListener("click", selecionaB, false);
+    torreC.torre.addEventListener("click", selecionaC, false);
+// }
+
+// window.addEventListener("load", iniciar, false);
+
+
 const dificuldade = document.getElementById("dificuldade");
 
 dificuldade.addEventListener("click", prueba);
 function prueba(event) {
+    limpaTorre();
     const select = event.target;
-    criaFicha(dificuldade.value);
+    guardaFichas(dificuldade.value);
+    
 }
 
-const reset = document.getElementById("reinicia");
+let fichaSelecionada;
+let origen;
+let destino;
 
-reset.addEventListener("click", limpaTorre);
+function elige(disco) {
+    if (disco.elegida) {
+        fichaSelecionada = disco.disco;
+    }else{
+        disco.disco.className = "quieta";
+    }
+}
+
+function seleciona(torre) {
+    if (torre.elegido) {
+        torre.torre.style.borderColor = "red";
+    }else{
+        torre.torre.style.borderColor = "black";
+    }
+}
+
+function selecionaA() {
+    torreA.elegido = !torreA.elegido;
+    seleciona(torreA);
+}
+
+function selecionaB() {
+    torreB.elegido = !torreB.elegido;
+    seleciona(torreB);
+}
+
+function selecionaC() {
+    torreC.elegido = !torreC.elegido;
+    seleciona(torreC);
+}
+
+function mueve(event) {
+    disco0.elegida = !disco0.elegida;
+    let selecionada = event.target;
+    if (disco0.elegida) {
+        torreB.torre.appendChild(selecionada)
+        disco0.elegida = false;
+    }
+    
+}
+
+function elige0() {
+    disco0.elegida = !disco0.elegida;
+    elige(ficha0);
+}
+
+function elige1() {
+    disco1.elegida = !disco1.elegida;
+    elige(ficha1);
+}
+
+
+
+ficha0.addEventListener("click", mueve, false);
+ficha1.addEventListener("click", mueve, false);
+ficha2.addEventListener("click", mueve, false);
+ficha3.addEventListener("click", mueve, false);
+ficha4.addEventListener("click", mueve, false);
+ficha5.addEventListener("click", mueve, false);
+ficha6.addEventListener("click", mueve, false);
+ficha7.addEventListener("click", mueve, false);
+
+
+
+// function selecionarFicha(event){
+//     fichaSelecionada = fichasA[0]
+//     fichaSelecionada = event.target;
+//     if (fichaSelecionada.className === "disk") {
+//         torreB.torre.appendChild(fichaSelecionada)
+//     }
+// }
+
+
+
+// const reset = document.getElementById("reinicia");
+
+// reset.addEventListener("click", limpaTorre);
+
+/*****************************************************************************/
+
+/*****************************************************************************/
+
+
+// const fichas = document.getElementsByClassName("disk");
+
+
+
+
+// discos[0].disco.addEventListener("click", selecionarFicha)
+
+// const size1 = disco1.getAttribute("width");
+// console.log(size1);
+
+ 
+
+
+
+
+
+// function origenDestino(ficha) {
+//     if (origen === undefined) {
+//         if (ficha.length !== 0) {
+//             origen = ficha;
+//             origen.elegido = true;
+//         }
+//     }else if (origen !== undefined && destino === undefined) {
+//         destino = ficha;
+//         destino.elegido = true;
+//         if (origen !== destino) {
+//             if (condition) {
+                
+//             }
+//         }
+//     }
+// }
+
+// function fichaSuperior() {
+//     for (let i = 0; i < fichas.length; i++) {
+//         const element = fichas[i];
+//         if (!(element instanceof Disco)) {
+//             return element
+//         }
+//     }
+// }
